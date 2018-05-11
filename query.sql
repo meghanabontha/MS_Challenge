db75cc9bfde57a238f1f8c6fcb9369903fe3c306
meghanabontha <36574736+meghanabontha@users.noreply.github.com>
2018-05-11 03:08:07 -0400

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,4 @@
+//This is for creating the table in the SQL database. All the fields are initialized accordingly. 
+
+create table table_data_csv(A varchar(50), B varchar(50), C varchar(50), D varchar950, E blob, F varchar(100),
+                            G varchar(100), H varchar(50), I varchar(100), J varchar(100));
